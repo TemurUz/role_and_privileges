df5ad65a923d4e3a1778bfdf62d8fe1ea1b02356
TemurUz <temirmaliknomozov@gmail.com>
2022-08-11 19:44:24 +0500

    role id add

--- src/main/java/temur/uz/springsecurityandrole/entity/Role.java
+++ src/main/java/temur/uz/springsecurityandrole/entity/Role.java
@@ -1,11 +1,11 @@
 package temur.uz.springsecurityandrole.entity;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Data
@@ -16,19 +16,16 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    @NotNull
     private String name;
 
-    @ManyToMany
-    @JoinTable(
-            name = "roles_privileges",
-            joinColumns = @JoinColumn(
-                    name = "role_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(
-                    name = "privilege_id", referencedColumnName = "id"))
-    private List<Privilege> privileges;
+//    @ManyToMany
+//    @JoinTable(
+//            name = "roles_privileges",
+//            joinColumns = @JoinColumn(
+//                    name = "role_id", referencedColumnName = "id"),
+//            inverseJoinColumns = @JoinColumn(
+//                    name = "privilege_id", referencedColumnName = "id"))
+//    private List<Privilege> privileges;
 
-    public Role(String name) {
-        this.name = name;
-    }
 }
